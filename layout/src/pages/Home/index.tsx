91625f06f298c485ba3f047d44b60c1b7ed8dc89
victor.silva <victor.silva@univision.com.br>
2023-02-10 12:18:47 -0300

    add: pagina home left

--- layout/src/pages/Home/index.tsx
+++ layout/src/pages/Home/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Container, Item, Left, Logo, Right } from './styles';
 
-import { Container } from './styles';
+import imgContato from '../../assets/contato.svg';
+import imgInicio from '../../assets/inicio.svg';
+import imgLogo from '../../assets/logo.svg';
+import imgRelatorios from '../../assets/relatorios.svg';
 
 const Home: React.FC = () => {
-  return <Container></Container>;
+  const items = [
+    { img: imgInicio, label: 'Inicio', active: true },
+    { img: imgContato, label: 'Contatos', active: false },
+    { img: imgRelatorios, label: 'Relatórios', active: false },
+    { img: imgContato, label: 'Contatos', active: false },
+    { img: imgContato, label: 'Contatos', active: false },
+    { img: imgContato, label: 'Contatos', active: false },
+  ];
+
+  return (
+    <Container>
+      <Left>
+        <Logo>
+          <img src={imgLogo} alt="tropa digital" />
+        </Logo>
+
+        {items.map((item, index) => (
+          <Item active={item.active} key={index}>
+            <img src={item.img} alt="inicio" />
+            <span>{item.label}</span>
+          </Item>
+        ))}
+      </Left>
+
+      <Right></Right>
+    </Container>
+  );
 };
 
 export default Home;
--- layout/src/pages/Home/styles.ts
+++ layout/src/pages/Home/styles.ts
@@ -1,5 +1,84 @@
-import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  active: boolean;
+}
 
 export const Container = styled.div`
-  background: #000;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Left = styled(motion.div)`
+  display: flex;
+  width: 20%;
+  height: 100%;
+
+  flex-direction: column;
+  align-items: flex-end;
+
+  padding: 60px 0;
+
+  background: #ffffff;
+`;
+
+export const Logo = styled(motion.div)`
+  display: grid;
+  width: 100%;
+
+  margin-bottom: 50px;
+  place-items: center;
+
+  img {
+    width: 65%;
+  }
+`;
+
+export const Item = styled(motion.div)<Props>`
+  display: flex;
+  width: 85%;
+  height: 54px;
+
+  justify-content: flex-start;
+  align-items: center;
+
+  padding: 8px;
+  margin: 8px 10px;
+
+  cursor: pointer;
+  border-radius: 5px;
+
+  color: #ffffff;
+  background: #2eafb2;
+
+  ${({ active }) =>
+    active &&
+    css`
+      width: 89%;
+
+      margin-right: 0;
+      border-radius: 5px 0 0 5px;
+
+      color: #000000;
+      background: #e5e5e5;
+    `};
+
+  img {
+    width: 22px;
+    margin-right: 12px;
+  }
+
+  span {
+    font-size: 16px;
+    font-weight: 500;
+  }
+`;
+
+export const Right = styled(motion.div)`
+  width: 80%;
+  height: 100%;
+
+  background: #e5e5e5;
 `;
